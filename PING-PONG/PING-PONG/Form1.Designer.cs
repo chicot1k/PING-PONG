0b14fd34aac6940869927a1e50e9fdb56164f173
chicot1k <58642288+chicot1k@users.noreply.github.com>
2020-05-27 17:41:50 +0300

    Add files via upload

---
+++ PING-PONG/PING-PONG/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PING_PONG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PLAYER2 = new System.Windows.Forms.Label();
+            this.PLAYER1 = new System.Windows.Forms.Label();
+            this.box = new System.Windows.Forms.PictureBox();
+            this.rule = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PL1W = new System.Windows.Forms.Label();
+            this.PL2W = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(351, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SCORE\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(69, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 68);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "PLAYER1 \r\n\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(604, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 34);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "PLAYER2";
+            // 
+            // PLAYER2
+            // 
+            this.PLAYER2.AutoSize = true;
+            this.PLAYER2.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PLAYER2.Location = new System.Drawing.Point(462, 9);
+            this.PLAYER2.Name = "PLAYER2";
+            this.PLAYER2.Size = new System.Drawing.Size(26, 34);
+            this.PLAYER2.TabIndex = 4;
+            this.PLAYER2.Text = "0";
+            // 
+            // PLAYER1
+            // 
+            this.PLAYER1.AutoSize = true;
+            this.PLAYER1.Font = new System.Drawing.Font("Agency FB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PLAYER1.Location = new System.Drawing.Point(250, 9);
+            this.PLAYER1.Name = "PLAYER1";
+            this.PLAYER1.Size = new System.Drawing.Size(26, 34);
+            this.PLAYER1.TabIndex = 5;
+            this.PLAYER1.Text = "0";
+            // 
+            // box
+            // 
+            this.box.Location = new System.Drawing.Point(-2, 57);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(800, 400);
+            this.box.TabIndex = 6;
+            this.box.TabStop = false;
+            // 
+            // rule
+            // 
+            this.rule.AutoSize = true;
+            this.rule.Font = new System.Drawing.Font("Agency FB", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rule.Location = new System.Drawing.Point(-2, 110);
+            this.rule.Name = "rule";
+            this.rule.Size = new System.Drawing.Size(806, 200);
+            this.rule.TabIndex = 7;
+            this.rule.Text = resources.GetString("rule.Text");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // PL1W
+            // 
+            this.PL1W.AutoSize = true;
+            this.PL1W.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PL1W.Location = new System.Drawing.Point(278, 57);
+            this.PL1W.Name = "PL1W";
+            this.PL1W.Size = new System.Drawing.Size(210, 58);
+            this.PL1W.TabIndex = 8;
+            this.PL1W.Text = "PLAYER1 WIN!";
+            this.PL1W.Visible = false;
+            // 
+            // PL2W
+            // 
+            this.PL2W.AutoSize = true;
+            this.PL2W.Font = new System.Drawing.Font("Agency FB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PL2W.Location = new System.Drawing.Point(278, 57);
+            this.PL2W.Name = "PL2W";
+            this.PL2W.Size = new System.Drawing.Size(221, 58);
+            this.PL2W.TabIndex = 9;
+            this.PL2W.Text = "PLAYER2 WIN!";
+            this.PL2W.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 457);
+            this.Controls.Add(this.PL2W);
+            this.Controls.Add(this.PL1W);
+            this.Controls.Add(this.rule);
+            this.Controls.Add(this.box);
+            this.Controls.Add(this.PLAYER1);
+            this.Controls.Add(this.PLAYER2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PING-PONG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label PLAYER2;
+        private System.Windows.Forms.Label PLAYER1;
+        private System.Windows.Forms.Label rule;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label PL1W;
+        private System.Windows.Forms.Label PL2W;
+    }
+}
+
---
+++ PING-PONG/PING-PONG/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PING_PONG
+{
+    public partial class Form1 : Form
+    {
+        const int absspeedbally = 4;//скорость по модулю 
+        const int xsizeplatform = 60;//размер платформы по х
+        const int ysizeplatform = 20;//размер платформы по у
+        const int yplatform1=0;//координаты  для платформы
+        const int sizeball = 10;// радиус мяча
+        const int xspeedplatform = 20;//скорость платформы
+        int yplatform2;//координаты  для второй платформы
+        bool stage = false;//оператор для понятие включена ли игра
+        int xball;//координаты мяча
+        int speedbally;//cкорость мяча по у
+        int speedballx;//скорость мяча по х
+        int yball; //координаты мяча
+        int xplatform1 ;// координаты для платофрмы по х
+        int xplatform2;// координаты для платфорымы по у
+        Bitmap bitmap;
+        Graphics graphics;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int key = e.KeyValue;
+            if (key == 32 && !stage) 
+            {
+                Random random = new Random();
+                speedbally = absspeedbally;
+                speedballx = random.Next(-4, 4);
+                if (speedballx == 0)
+                {
+                    speedballx = 2;
+                }
+                rule.Visible = false;
+                stage = true;
+                timer1.Start();
+                PL1W.Visible = false;
+                PL2W.Visible = false;
+            }
+            if (key == 65 && stage)
+            {
+                if  (xplatform1-xspeedplatform>-20)
+                {
+                    xplatform1 -= xspeedplatform;
+                } 
+            }
+            if (key == 68 && stage)
+            {
+                if ((xplatform1 + xsizeplatform) + xspeedplatform<box.Width+20)
+                {
+                    xplatform1 += xspeedplatform;
+                }
+            }
+            if(key == 37 && stage)
+            {
+                if (xplatform2 + xspeedplatform > 20)
+                {
+                    xplatform2 -= xspeedplatform;
+                }
+            }
+            if(key == 39 && stage)
+            {
+                if ((xplatform2 + xsizeplatform) + xspeedplatform < Width)
+                {
+                    xplatform2 += xspeedplatform;
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(box.Width, box.Height);
+            
+            
+            yplatform2 = box.Height - ysizeplatform;
+            xplatform1 = box.Width / 2 - xsizeplatform / 2;
+            xplatform2 = xplatform1;
+            xball = box.Width / 2-sizeball/2;
+            yball = box.Height / 2-sizeball/2;
+            graphics = Graphics.FromImage(bitmap);
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            xball += speedballx;
+            yball += speedbally;
+            if (xball < 0 || xball+2*sizeball>box.Width)
+            {
+                speedballx *= -1;
+            }
+            else if(yball<ysizeplatform && xball+2*sizeball>xplatform1 && xball < xplatform1 + xsizeplatform)
+            {
+                speedbally *= -1;
+
+            }
+            else if(yball+2 * sizeball > box.Height-ysizeplatform && xball+ 2 * sizeball > xplatform2 && xball < xplatform2 + xsizeplatform)
+            {
+                    speedbally *= -1;
+
+            }
+            else if (yball < 0)
+            {
+                PLAYER2.Text = (int.Parse(PLAYER2.Text) + 1).ToString();
+                yplatform2 = box.Height - ysizeplatform;
+                xplatform1 = box.Width / 2 - xsizeplatform / 2;
+                xplatform2 = xplatform1;
+                xball = box.Width / 2 - sizeball / 2;
+                yball = box.Height / 2 - sizeball / 2;
+                Random random = new Random();
+                speedbally = absspeedbally;
+                speedballx = random.Next(-4, 4);
+                if (speedballx == 0)
+                {
+                    speedballx = 2;
+                }
+            }
+            else if(yball > box.Height)
+            {
+                PLAYER1.Text = (int.Parse(PLAYER1.Text) + 1).ToString();
+                yplatform2 = box.Height - ysizeplatform;
+                xplatform1 = box.Width / 2 - xsizeplatform / 2;
+                xplatform2 = xplatform1;
+                xball = box.Width / 2 - sizeball / 2;
+                yball = box.Height / 2 - sizeball / 2;
+                Random random = new Random();
+                speedbally = absspeedbally;
+                speedballx = random.Next(-4, 4);
+                if (speedballx == 0)
+                {
+                    speedballx = 2;
+                }
+            }
+            if (PLAYER1.Text == "10")
+            {
+                PLAYER1.Text = "0";
+                PLAYER2.Text = "0";
+                rule.Visible = true;
+                timer1.Stop();
+                stage = false;
+                PL1W.Visible = true;
+            }
+            if (PLAYER2.Text == "10")
+            {
+                PLAYER2.Text = "0";
+                PLAYER1.Text = "0";
+                rule.Visible = true;
+                timer1.Stop();
+                stage = false;
+                PL2W.Visible = true;
+            }
+
+            graphics.Clear(Color.White);
+            graphics.FillRectangle(Brushes.Red, xplatform1, yplatform1, xsizeplatform, ysizeplatform);
+            graphics.FillRectangle(Brushes.Blue, xplatform2, yplatform2, xsizeplatform, ysizeplatform);
+            graphics.FillEllipse(Brushes.YellowGreen,  xball,  yball,  2*sizeball, 2*sizeball);
+
+
+            box.Image = bitmap;
+        }
+    }
+}
